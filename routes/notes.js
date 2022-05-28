796ef307dbc209f1842b9faf2b72cdc6d2aa47f1
AliBakercodes <alibakerconsulting@gmail.com>
2022-05-28 01:55:15 -0400

    Get and post working

--- routes/notes.js
+++ routes/notes.js
@@ -1,13 +1,23 @@
 const notes = require('express').Router();
+const fs = require('fs');
+const { fstat } = require('fs');
 const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
 const uuid = require('../helpers/uuid');
 
 
 // Get Route for retriving notes.html
 notes.get('/', (req, res) => {
-  console.info(`${req.method} request received for index`);
-
-  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+  console.info(`${req.method} request received for notes`);
+  
+  readFromFile('./db/db.json').then((data) => {
+    try {
+      res.json(JSON.parse(data));
+    } catch(err) {
+      fs.writeFile('./db/db.json','[]', (err) =>
+      err ? console.error(err) : console.log('Success!'));
+      console.log(err)
+    }
+  });
 });
 
 // POST Route for submitting notes
@@ -16,18 +26,49 @@ notes.post('/', (req, res) => {
   console.info(`${req.method} request received to submit notes`);
 
   // Destructuring assignment for the items in req.body
-  const { id, title, text } = req.body;
-
+  const {title, text } = req.body;
+  console.log(title)
+  console.log(text)
   // If all the required properties are present
-  if (id && title && text) {
+  if (title && text) {
     // Variable for the object we will save
     const newNote = {
       id: uuid(),
       title,
       text,
     };
+    console.log(newNote);
+    readAndAppend(newNote, './db/db.json');
 
-    readAndAppend(newFeedback, './db/db.json');
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    res.json(response);
+  } else {
+    res.json('Error in posting note');
+  }
+});
+
+notes.delete('/', (req, res) => {
+  // Log that a POST request was received
+  console.info(`${req.method} request received to delete notes`);
+
+  // Destructuring assignment for the items in req.body
+  const {title, text } = req.body;
+  console.log(title)
+  console.log(text)
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save
+    const newNote = {
+      id: uuid(),
+      title,
+      text,
+    };
+    console.log(newNote);
+    readAndAppend(newNote, './db/db.json');
 
     const response = {
       status: 'success',
